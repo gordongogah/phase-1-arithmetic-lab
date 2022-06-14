1485195c4a429c324f2bf1aa5529c59785acb0c6
gordongogah <gordongogah9@gmail.com>
2022-06-14 22:56:58 +0300

    commit tree 2c691c7304d95c87bd25e2c5fad1e8a74bd23ff1

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 24;
+let num4 = 5;
+let multiply = num1 * num2
+let random = Math.floor(Math.random()*10) + 1
+let mod = num3 % num4
+let max = Math.max(20, 19, 5,15,8)
 
-// Write your code here
+console.log(random)
